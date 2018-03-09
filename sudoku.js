ffdd59e7cb2d5f87a4aa08f715e1669e6af9def7
Tbfaldhi <tubagusfaldhi@gmail.com>
2018-03-09 18:21:26 +0700

    cetak board dgn angka dan batas garis

--- sudoku.js
+++ sudoku.js
@@ -1,12 +1,39 @@
 "use strict"
 
 class Sudoku {
-  constructor(board_string) {}
+  constructor(board_string) {
+    this.unsolved=board_string
+
+  }
 
   solve() {}
 
   // Returns a string representing the current state of the board
-  board() {}
+  board() {
+    var str = this.unsolved
+    var kotak=[]
+    var index=0
+    for (var i = 0; i < 12; i++) {
+      let kotakTmp=[]
+      if(i==3||i==7||i==11){
+        for (var k = 0; k < 11; k++) {
+          kotakTmp.push('_')
+        }
+      }else{
+        for (var k = 0; k < 11; k++) {
+          if(k==3||k==7){
+          kotakTmp.push('|')
+        }else{
+          kotakTmp.push(str[index])
+          index++
+        }
+        }
+      }
+      kotak.push(kotakTmp.join(' '))
+    }
+      return kotak.join('\n')
+  }
+
 }
 
 // The file has newlines at the end of each line,
@@ -17,7 +44,7 @@ var board_string = fs.readFileSync('set-01_sample.unsolved.txt')
   .split("\n")[0]
 
 var game = new Sudoku(board_string)
-
+console.log(game)
 // Remember: this will just fill out what it can and not "guess"
 game.solve()
 
